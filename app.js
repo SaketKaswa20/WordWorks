b7faf1b70199ee44bf9ba38302669c1eaee4326b
Saket Kaswa <105808363+SaketKaswa20@users.noreply.github.com>
2023-07-20 22:39:46 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const inputText = document.getElementById("inputText");
+  const countButton = document.getElementById("countButton");
+  const upperButton = document.getElementById("upperButton");
+  const lowerButton = document.getElementById("lowerButton");
+  const capitalizeButton = document.getElementById("capitalizeButton");
+  const removeExtraSpacesButton = document.getElementById("removeExtraSpacesButton");
+  const wordCount = document.getElementById("wordCount");
+
+  countButton.addEventListener("click", () => {
+    const text = inputText.value;
+    const words = text.trim().split(/\s+/).length;
+    wordCount.textContent = `Word count: ${words}`;
+  });
+
+  upperButton.addEventListener("click", () => {
+    inputText.value = inputText.value.toUpperCase();
+  });
+
+  lowerButton.addEventListener("click", () => {
+    inputText.value = inputText.value.toLowerCase();
+  });
+
+  capitalizeButton.addEventListener("click", () => {
+    const text = inputText.value;
+    inputText.value = text.replace(/\b\w/g, (match) => match.toUpperCase());
+  });
+
+  removeExtraSpacesButton.addEventListener("click", () => {
+    const text = inputText.value;
+    inputText.value = text.replace(/\s+/g, " ").trim();
+  });
+});
